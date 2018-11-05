557c7787bc688da9edb0e880b670b3232e6bc3a4
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2018-11-05 20:54:31 +0200

    initialise game loop

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,26 @@
 module Main where
+import           FRP.Yampa                          (Event (..), SF, arr, tag,
+                                                     (>>>))
+import           Graphics.Gloss
+import           Graphics.Gloss.Interface.FRP.Yampa
+import qualified Graphics.Gloss.Interface.IO.Game   as G
+import           System.Random                      (StdGen, newStdGen)
+
+import           Types
+
+--------------------------------------------------------------------------------
+
+-- something like a game loop in other languages
+-- for handling overall game process, parsing the input, initializing game logic
+-- drawing the resulting game state
+mainSignal :: StdGen -> SF (Event InputEvent) Picture
+mainSignal g = undefined
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  r <- newStdGen
+  playYampa
+   (InWindow "Sugarman" (410, 500) (200, 200))
+   white
+   30
+   (mainSignal g)
---
+++ src/Types/Obstacle.hs
@@ -0,0 +1,12 @@
+module Types where
+
+
+
+--------------------------------------
+-- Core types
+
+-- 1. Hero
+-- 2. Obstacles
+-- 2.1 Different types of obstacles
+-- 3. Surface
+-- 3.1 Different types of surface
